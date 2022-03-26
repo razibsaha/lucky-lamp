042d9bbf4f9216343987ab840d42bc58d85491ce
Razib Saha <rajibpay@gmail.com>
2022-03-26 20:21:40 +0600

    Header Added

---
+++ src/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <div className="product-container">
+                Choose your lamps
+            </div>
+            <div className="cart-container">
+                Order Summary
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
